3aca7b6a7e3670a008736b8cf65c70727c627a71
HnZhang <hnZhang0_0@163.com>
2019-08-26 00:08:43 +0800

    update publicPath to '/cv/'

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import CV from './views/CV.vue'
 
 Vue.use(Router)
 
@@ -8,26 +8,10 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // },
     {
       path: '/',
       name: 'cv',
-      // route level code-splitting
-      // this generates a separate chunk (cv.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "cv" */ './views/CV.vue')
-    }
+      component: CV
+    },
   ]
 })
